d5f9bc07987eefd09c90010aa739a4c5016760ea
mplank <michael.plank@preis.de>
2023-12-27 00:28:34 +0100

    add tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+    },
+    },
   },
-  plugins: [],
+  plugins: [forms],
 };
